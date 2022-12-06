432d3b37919354b1a03fd841de235c50123b4151
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2022-12-05 22:44:53 -0700

    Automate Employee Onboarding Script

---
+++ Flyhomes Automation.ps1
@@ -0,0 +1,18 @@
+﻿#ECHO Off
+
+# Script:                       Ops 201 d7 Flyhomes Final
+# Author:                       Kevin Isaac
+# Date of latest revision:      20221205
+# Purpose:      Automate Employee Onboarding Process
+
+$name= Read-Host -Prompt "Enter Employee Name"
+
+md C:\Users\sytyg\Desktop\$name
+md C:\Users\sytyg\Desktop\$name\Shipping
+md C:\Users\sytyg\Desktop\$name\Flyhomes_SOPs
+md C:\Users\sytyg\Desktop\$name\New_Employee_Orientation
+md C:\Users\sytyg\Desktop\$name\TPS_Reports
+md C:\Users\sytyg\Desktop\$name\Blueprints
+md C:\Users\sytyg\Desktop\$name\Model_Homes
+
+copy C:\Users\sytyg\Desktop\filestocopy\*.* C:\Users\sytyg\Desktop\$name\New_Employee_Orientation
